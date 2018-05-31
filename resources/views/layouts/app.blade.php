4f30f3629b5617c3e93448c479eba6cf5f02c01e
Shafik Shaon <shafikshaon@gmail.com>
2018-06-01 00:13:09 +0600

    profile dashboard view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,15 +72,18 @@
                 </a>
 
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                       onclick="event.preventDefault();
-                                     document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
+                  <a class="dropdown-item" href="{{ route('profile') }}">
+                    {{ __('Profile') }}
+                  </a>
+                  <a class="dropdown-item" href="{{ route('logout') }}"
+                     onclick="event.preventDefault();
+                                   document.getElementById('logout-form').submit();">
+                      {{ __('Logout') }}
+                  </a>
 
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      @csrf
+                  </form>
                 </div>
             </li>
             @else
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,6 @@ $this->post('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Front end
 Route::get('donors', 'HomeController@getListOfDonors')->name('list_of_donors');
+
+// Profile
+Route::get('profile', 'ProfileController@getProfile')->name('profile')->middleware('auth');
